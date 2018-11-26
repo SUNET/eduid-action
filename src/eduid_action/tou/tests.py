36c90d8add60b1feacb334b791434cf9775718f3
Johan Lundberg <lundberg@sunet.se>
2018-11-26 18:27:33 +0100

    Do not get TOUs over http any more

--- src/eduid_action/tou/tests.py
+++ src/eduid_action/tou/tests.py
@@ -110,14 +110,7 @@ class ToUActionPluginTests(ActionsTestCase):
                     data = json.loads(response.data)
                     self.assertEquals(data['action'], False)
 
-    @patch('eduid_action.tou.action.http.request')
-    def test_get_config(self, mock_http_request):
-        data = json.dumps({'payload':{
-                               'en': 'testing the ToU',
-                               'sv': 'testa ToU'}
-                               })
-        resp = Response(response=data, status=200, mimetype='application/json')
-        mock_http_request.return_value = resp
+    def test_get_config(self):
         with self.session_cookie(self.browser) as client:
             with client.session_transaction() as sess:
                 with self.app.test_request_context():
@@ -128,32 +121,13 @@ class ToUActionPluginTests(ActionsTestCase):
                     self.assertEqual(response.status_code, 200)
                     response = client.get('/config')
                     data = json.loads(response.data)
-                    self.assertEquals(data['payload']['tous']['sv'], 'testa ToU')
+                    self.assertEquals(data['payload']['tous']['sv'], 'test tou svenska')
 
-    @patch('eduid_action.tou.action.http.request')
-    def test_get_config_302_tous(self, mock_http_request):
-        resp = Response(status=302, mimetype='application/json')
-        mock_http_request.return_value = resp
+    def test_get_config_no_tous(self):
         with self.session_cookie(self.browser) as client:
             with client.session_transaction() as sess:
                 with self.app.test_request_context():
-                    mock_idp_app = MockIdPApp(self.app.actions_db, tou_version='test-version')
-                    add_actions(mock_idp_app, self.user, None)
-                    self.authenticate(client, sess)
-                    response = client.get('/get-actions')
-                    self.assertEqual(response.status_code, 200)
-                    response = client.get('/config')
-                    data = json.loads(response.data)
-                    self.assertEquals(data['payload']['message'], 'tou.no-tou')
-
-    @patch('eduid_action.tou.action.http.request')
-    def test_get_config_500_tous(self, mock_http_request):
-        resp = Response(status=500, mimetype='application/json')
-        mock_http_request.return_value = resp
-        with self.session_cookie(self.browser) as client:
-            with client.session_transaction() as sess:
-                with self.app.test_request_context():
-                    mock_idp_app = MockIdPApp(self.app.actions_db, tou_version='test-version')
+                    mock_idp_app = MockIdPApp(self.app.actions_db, tou_version='not-existing-version')
                     add_actions(mock_idp_app, self.user, None)
                     self.authenticate(client, sess)
                     response = client.get('/get-actions')
